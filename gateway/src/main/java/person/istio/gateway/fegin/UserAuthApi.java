c60ad38e26f871c96d7ca0df8cbbdcb847eeddbc
esc2333 <517478091@qq.com>
2022-11-16 17:28:36 +0800

    init

--- gateway/src/main/java/person/istio/gateway/fegin/UserAuthApi.java
+++ gateway/src/main/java/person/istio/gateway/fegin/UserAuthApi.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import person.istio.gateway.fegin.fallback.UserAuthApiFallback;
 
-@FeignClient(name = "user",url = "${route.user.url}",fallback = UserAuthApiFallback.class)
+//@FeignClient(name = "user",url = "${route.user.url}",fallback = UserAuthApiFallback.class)
+@FeignClient(name = "istio-user",fallback = UserAuthApiFallback.class)
 public interface UserAuthApi {
     @RequestMapping(value = "/auth/login", method = RequestMethod.POST)
     String login(@RequestParam("username") String username, @RequestParam("password") String password);
